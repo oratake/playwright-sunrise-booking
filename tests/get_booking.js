d57454a9ba9436dc5318047c53a93734fb592379
oratake <smith.shimomura@gmail.com>
2024-05-19 09:15:35 +0900

    WIP: nodeサーバ準備

--- tests/get_booking.js
+++ tests/get_booking.js
@@ -1,9 +1,47 @@
 const playwright = require('playwright');
 const https = require('https');
+// const http = require('http');
+// const url = require('url');
 
-(async () => {
+// console.log('server started.');
+// const server = http.createServer((req, res) => {
+//   // リクエストURLを解析
+//   const parsedUrl = url.parse(req.url, true);
+// 
+//   // GETリクエストの場合
+//   if (req.method === 'GET') {
+//     console.log('get request detected', parsedUrl.query);
+//     // クエリパラメータを取得
+//     const { date, hour, minute, depStName, arrStName, trainName } = parsedUrl.query;
+//     // 予約確認を実行
+//     const result = fetchTrainBookingInfo(date, hour, minute, depStName, arrStName, trainName);
+// 
+//     // レスポンスを返す
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.end(JSON.stringify(result));
+//     // // エラーレスポンスを返す
+//     // res.statusCode = 400;
+//     // res.setHeader('Content-Type', 'application/json');
+//     // res.end(JSON.stringify({ error: 'Invalid parameters' }));
+//   } else {
+//     // その他のリクエストメソッドの場合
+//     res.statusCode = 404;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.end(JSON.stringify({ error: 'Not found' }));
+//   }
+// });
+// 
+// server.listen(3000, () => {
+//   console.log('Server running at http://localhost:3000/');
+// });
+
+fetchTrainBookingInfo();
+// async function fetchTrainBookingInfo(date, hour, minute, depStName, arrStName, trainName) {
+async function fetchTrainBookingInfo() {
   const browser = await playwright['chromium'].launch({
-    headless: false,
+    // 確認時のみheadless flaseに
+    // headless: false,
   });
   const page = await browser.newPage();
 
@@ -18,30 +56,39 @@ const https = require('https');
     'seto': '%BB%BE%C4%BB%20000',
     'izumo': '%BB%B2%BD%D3%BB000',
   };
-  const inputData = {
-    'date': '20240608',
-    'hour': '21',
-    'minute': '00',
-    'depStName': 'osaka',
-    'arrStName': 'tokyo',
-    'trainName': 'izumo',
-  };
 
-  const url = `https://e5489.jr-odekake.net/e5489/cspc/CBDayTimeArriveSelRsvMyDiaPC?inputDepartStName=${stName[inputData.depStName]}&inputArriveStName=${stName[inputData.arrStName]}&inputType=0&inputDate=${inputData.date}&inputHour=${inputData.hour}&inputMinute=${inputData.minute}&inputUniqueDepartSt=1&inputUniqueArriveSt=1&inputSearchType=2&inputTransferDepartStName1=${stName[inputData.depStName]}&inputTransferArriveStName1=${stName[inputData.arrStName]}&inputTransferDepartStUnique1=1&inputTransferArriveStUnique1=1&inputTransferTrainType1=0001&inputSpecificTrainType1=2&inputSpecificBriefTrainKana1=${trainKana[inputData.trainName]}&SequenceType=0&inputReturnUrl=goyoyaku/campaign/sunriseseto_izumo/form.html&RTURL=https://www.jr-odekake.net/goyoyaku/campaign/sunriseseto_izumo/form.html&undefined`
+  // const inputData = {
+  //   'date': '20240608',
+  //   'hour': '21',
+  //   'minute': '00',
+  //   'depStName': 'osaka',
+  //   'arrStName': 'tokyo',
+  //   'trainName': 'izumo',
+  // };
+  const date = '20240608';
+  const hour = '21';
+  const minute = '00';
+  const depStName = 'osaka';
+  const arrStName = 'tokyo';
+  const trainName = 'izumo';
+
+  const url = `https://e5489.jr-odekake.net/e5489/cspc/CBDayTimeArriveSelRsvMyDiaPC?inputDepartStName=${stName[depStName]}&inputArriveStName=${stName[arrStName]}&inputType=0&inputDate=${date}&inputHour=${hour}&inputMinute=${minute}&inputUniqueDepartSt=1&inputUniqueArriveSt=1&inputSearchType=2&inputTransferDepartStName1=${stName[depStName]}&inputTransferArriveStName1=${stName[arrStName]}&inputTransferDepartStUnique1=1&inputTransferArriveStUnique1=1&inputTransferTrainType1=0001&inputSpecificTrainType1=2&inputSpecificBriefTrainKana1=${trainKana[trainName]}&SequenceType=0&inputReturnUrl=goyoyaku/campaign/sunriseseto_izumo/form.html&RTURL=https://www.jr-odekake.net/goyoyaku/campaign/sunriseseto_izumo/form.html&undefined`
   await page.goto(url);
 
-  // テストJSON
   const postData = {
-    "bookingDate": inputData.date,
-    "depSt": inputData.depStName,
-    "arrSt": inputData.arrStName,
-    "trainName": inputData.trainName,
+    "bookingDate": date,
+    "depSt": depStName,
+    "arrSt": arrStName,
+    "trainName": trainName,
     "html": await page.locator('table.seat-facility').innerHTML()
   };
+  console.log('post spreadsheet', postData);
   await postSpreadsheet(postData);
 
   await browser.close();
-})();
+
+  return { result: 'Success' };
+}
 
 function postSpreadsheet(jsonData) {
   const options = {
